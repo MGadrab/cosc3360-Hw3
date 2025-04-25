e4198c13aa7c8dbae7b6cb5e1771a29054757113
MGadrab <mgadrab@gmail.com>
2025-04-25 00:46:29 -0500

    added main.cpp and main.h file

---
+++ page_replacement.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mohammed Gadrab on 4/25/25.
+//
+
+#include "page_replacement.h"
---
+++ page_replacement.h
@@ -0,0 +1,14 @@
+//
+// Created by Mohammed Gadrab on 4/25/25.
+//
+
+#ifndef COSC3360_HW3_PAGE_REPLACEMENT_H
+#define COSC3360_HW3_PAGE_REPLACEMENT_H
+
+
+class page_replacement {
+
+};
+
+
+#endif //COSC3360_HW3_PAGE_REPLACEMENT_H
